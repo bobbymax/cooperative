8e0d222e76c7a30b7302f2d57affa72aa370b802
TimGabriel1 <zinwotatimothy@gmail.com>
2021-10-20 15:45:02 +0100

    Documenting Endpoints with Swagger

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,55 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-  /**
-     * @OA\Post(
-     ** path="/login",
-     *   tags={"Auth"},
-     *   summary="Sign in as an existing user",
-     *   operationId="authlogin",
-     *   security={ {"bearer": {} }},
-     *
-     *   @OA\Parameter(
-     *      name="email",
-     *      in="query",
-     *      required=true,
-     *      @OA\Schema(
-     *           type="string"
-     *      )
-     *   ),
-     *   @OA\Parameter(
-     *      name="password",
-     *      in="query",
-     *      required=true,
-     *      @OA\Schema(
-     *          type="string"
-     *      )
-     *   ),
-     *   @OA\Response(
-     *      response=200,
-     *       description="Success",
-     *      @OA\MediaType(
-     *           mediaType="application/json",
-     *      )
-     *   ),
-     *   @OA\Response(
-     *      response=401,
-     *       description="Unauthenticated"
-     *   ),
-     *   @OA\Response(
-     *      response=400,
-     *      description="Bad Request"
-     *   ),
-     *   @OA\Response(
-     *      response=404,
-     *      description="not found"
-     *   ),
-     *      @OA\Response(
-     *          response=403,
-     *          description="Forbidden"
-     *      )
-     *)
-     */
+/**
+ * @OA\Post(
+ * path="/login",
+ * summary="Sign in",
+ * description="Login by email, password",
+ * operationId="authLogin",
+ * tags={"auth"},
+ * @OA\RequestBody(
+ *    required=true,
+ *    description="Pass user credentials",
+ *    @OA\JsonContent(
+ *       required={"email","password"},
+ *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+ *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+ *
+ *    ),
+ * ),@OA\Response(
+*     response=200,
+*     description="Success",
+*     @OA\JsonContent(
+*        @OA\Property(property="user", type="object", ref="#/components/schemas/User"),
+*     )
+*  ),
+ * @OA\Response(
+ *    response=422,
+ *    description="Wrong credentials response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+ *        )
+ *     )
+ * )
+ */
 
 class LoginController extends Controller
 {
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,22 @@ use Illuminate\Notifications\Notifiable;
 // use Laravel\Sanctum\HasApiTokens;
 use Laravel\Passport\HasApiTokens;
 
+/**
+ *
+ * @OA\Schema(
+ * required={"password"},
+ * @OA\Xml(name="User"),
+ * @OA\Property(property="company_id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="name", type="string", readOnly="true", description="User role", example= "John Doe") ,
+ * @OA\Property(property="email", type="string", readOnly="true", format="email", description="User unique email address", example="user@gmail.com"),
+ * @OA\Property(property="email_verified_at", type="string", readOnly="true", format="date-time", description="Datetime marker of verification status", example="2019-02-25 12:59:20"),
+ * @OA\Property(property="grade_level_id", type="string", maxLength=32, example="32"),
+ * @OA\Property(property="group", type="string", maxLength=32, example="Directors")
+ *
+ * )
+ * Class User
+ *
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
